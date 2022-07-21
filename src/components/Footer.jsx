883890105d4d30b471c9ba86418a3ec8b702a3ee
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-07-21 11:33:56 -0400

    fixing all the connections to deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/components/Footer.css';
 const Footer = () => {
     return (
 <div className="Footer">
-       <p className="Footer-title">SSGLOW</p>
+       <p className="Footer-title">Platzi Conf</p>
        <p className="Footer-copy">Todos los derechos reservados.</p>
 </div>
 
